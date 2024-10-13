2810a3b18f6c416b6724b042b17a1ccf96257ea4
Tsk <juliussan96@outlook.com>
2024-10-14 01:36:06 +0800

    Added Api-Side Delete functions for users

--- Backend-src/serverSrc.js
+++ Backend-src/serverSrc.js
@@ -5,30 +5,42 @@ import User from "./Models/users.model.js";
 
 dotenv.config();
 const app = express();
-app.use(express.json()); // json middleware
+app.use(express.json()); // json middleware processing 
 
+// backend post request to register a user 
 app.post("/api/users", async (req, res) => {
   const user = req.body;
 
+
   if (!user.name || !user.email || !user.password) {
-    return res.status(400).json({ msg: "Please enter all fields" });
+    return res.status(400).json({ success: false, msg: "Please enter all fields" });
   }
 
   const newUser = new User(user);
 
   try {
     await newUser.save();
-    res
-      .status(201)
-      .json({
+    res.status(201).json({
         success: true,
         data: newUser,
         msg: "User registered successfully",
       });
+
   } catch (error) {
     console.error("Error, user not registered", error.message);
     res.status(500).json({ success: false, msg: "Server Error" });
   }
+
+});
+
+// backend delete request to delete a user
+app.delete ("/api/users/:id", async (req, res) => {
+  const {id} = req.params;
+
+  try {
+    await User.findByIdAndDelete(id); 
+    res.status(200).json({ success: true, msg: "User deleted successfully" });
+  } catch (error) {} 
 });
 
 app.listen(6969, () => {
